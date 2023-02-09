bf119cfde2d86e4d1f805a73348751b771c3bd6c
karaxuna <kaxabernet@gmail.com>
2023-02-09 21:20:05 +0400

    add logs

--- src/ddns.ts
+++ src/ddns.ts
@@ -5,7 +5,10 @@ interface UpdateDNSRecordOptions {
 }
 
 export const updateDNSRecord = async (domainName: string, { token }: UpdateDNSRecordOptions) => {
-    const rootDomainName = domainName.split('.').splice(-2).join('.');
+    const rootDomainName = domainName
+        .split('.')
+        .splice(-2)
+        .join('.');
 
     const headers = {
         'Content-Type': 'application/json',
@@ -18,12 +21,14 @@ export const updateDNSRecord = async (domainName: string, { token }: UpdateDNSRe
         },
     } = await axios.get(
         `https://api.digitalocean.com/v2/domains/${rootDomainName}/records`,
-        { headers }
+        { headers },
     );
 
-    const record = domain_records.find(
-        record => record.name === domainName && record.type === 'A'
-    );
+    console.log('records:', domain_records);
+
+    const record = domain_records.find((record) => {
+        return record.name === domainName && record.type === 'A';
+    });
 
     if (!record) {
         throw new Error(`No A record found with name ${domainName}`);
