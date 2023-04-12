974315272f9e726b8089b3e37b6eed2e782fc427
Kakhaber <kaxabernet@gmail.com>
2023-04-12 22:21:53 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -130,11 +130,12 @@ export const startDdnsJob = async ({
 export const updateCerts = async () => {
   const ls = spawn(
     'bash',
-    [path.resolve(__dirname, '../acme.sh/acme.sh'), '--issue', '-d', process.env.TLD, '-d', `*.${process.env.TLD}`, '--dns', 'dns_dgon', '--log', '--register-account', '-m', 'info@shoty.co.uk'],
+    [path.resolve(__dirname, '../acme.sh/acme.sh'), '--issue', '-d', process.env.TLD, '-d', `*.${process.env.TLD}`, '--dns', 'dns_dgon', '--log'],
     {
       env: {
         ...process.env,
         DO_API_KEY: process.env.DIGITALOCEAN_API_TOKEN,
+        ACCOUNT_THUMBPRINT: process.env.ACCOUNT_THUMBPRINT,
       },
     },
   );
