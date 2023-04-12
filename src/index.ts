42ce8d01cd26822d793b589dd93ef7afb574b0ae
Kakhaber <kaxabernet@gmail.com>
2023-04-12 22:39:25 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -130,7 +130,7 @@ export const startDdnsJob = async ({
 export const updateCerts = async () => {
   const ls = spawn(
     'bash',
-    [path.resolve(__dirname, '../acme.sh/acme.sh'), '--issue', '-d', process.env.TLD, '-d', `*.${process.env.TLD}`, '--dns', 'dns_dgon', '--log'],
+    [path.resolve(__dirname, '../acme.sh/acme.sh'), '--issue', '-d', process.env.TLD, '-d', `*.${process.env.TLD}`, '--dns', 'dns_dgon', '--log', '-m', process.env.ZEROSSL_EMAIL],
     {
       env: {
         ...process.env,
